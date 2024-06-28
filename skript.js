7a297d7f6c4272f356dda3c438f4f4d6c5746431
xSAPSANx <misterdinal2000@mail.ru>
2024-06-28 18:47:05 +0300

    create adaptive swiper popular

--- skript.js
+++ skript.js
@@ -7,3 +7,34 @@ document.querySelector('.header_burger').addEventListener('click', function () {
 	document.querySelector('.header_menu-icon4').classList.toggle('open')
 	document.querySelector('.header_menu-icon5').classList.toggle('open')
 })
+
+var swiper = new Swiper('.mySwiper', {
+	slidesPerView: 3,
+	resistanceRatioX: 0.8,
+	direction: 'horizontal',
+	freeMode: true,
+	spaceBetween: 20,
+	initialSlide: 0,
+	keyboard: true,
+	autoplay: {
+		delay: 5000,
+		disableOnInteraction: false,
+	},
+	breakpoints: {
+		// when window width is >= 320px
+		400: {
+			slidesPerView: 1,
+			spaceBetween: 10,
+		},
+		// when window width is >= 480px
+		600: {
+			slidesPerView: 2,
+			spaceBetween: 20,
+		},
+		// when window width is >= 640px
+		1000: {
+			slidesPerView: 3,
+			spaceBetween: 20,
+		},
+	},
+})
