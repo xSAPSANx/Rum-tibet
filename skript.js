04d95cf92da31afc7350c7b205bb36cddd130c98
xSAPSANx <misterdinal2000@mail.ru>
2024-07-06 14:07:44 +0300

    create swiper gallary

--- skript.js
+++ skript.js
@@ -35,32 +35,78 @@ var swiper = new Swiper('.mySwiper', {
 		},
 	},
 })
-// if (document.documentElement.clientWidth < 768) {
-// 	var swiper = new Swiper('.mySwiperTravel', {
-// 		slidesPerView: 3,
-// 		resistanceRatioX: 0.8,
-// 		direction: 'horizontal',
-// 		freeMode: true,
-// 		spaceBetween: 24,
-// 		initialSlide: 0,
-// 		keyboard: true,
-// 		autoplay: {
-// 			delay: 5000,
-// 			disableOnInteraction: false,
-// 		},
-// 		breakpoints: {
-// 			300: {
-// 				slidesPerView: 1,
-// 				spaceBetween: 24,
-// 			},
-// 			600: {
-// 				slidesPerView: 2,
-// 				spaceBetween: 24,
-// 			},
-// 			1000: {
-// 				slidesPerView: 3,
-// 				spaceBetween: 24,
-// 			},
-// 		},
-// 	})
-// }
+
+window.addEventListener('DOMContentLoaded', () => {
+	const resizableSwiper = (
+		breakpoint,
+		swiperClass,
+		swiperSettings,
+		callback
+	) => {
+		let swiper
+
+		breakpoint = window.matchMedia(breakpoint)
+
+		const enableSwiper = function (className, settings) {
+			swiper = new Swiper(className, settings)
+
+			if (callback) {
+				callback(swiper)
+			}
+		}
+
+		const checker = function () {
+			if (breakpoint.matches) {
+				return enableSwiper(swiperClass, swiperSettings)
+			} else {
+				if (swiper !== undefined) swiper.destroy(true, true)
+				return
+			}
+		}
+
+		breakpoint.addEventListener('change', checker)
+		checker()
+	}
+
+	const someFunc = instance => {
+		if (instance) {
+			instance.on('slideChange', function (e) {
+				console.log('*** mySwiper.activeIndex', instance.activeIndex)
+			})
+		}
+	}
+
+	resizableSwiper('(max-width: 768px)', '.gallery__swiper', {
+		slidesPerView: 'auto',
+		spaceBetween: 10,
+		breakpoints: {
+			390: {
+				spaceBetween: 24,
+			},
+			350: {
+				spaceBetween: 5,
+			},
+		},
+		autoplay: {
+			delay: 5e3,
+			disableOnInteraction: !1,
+		},
+	})
+
+	resizableSwiper('(max-width: 768px)', '.travel_swiper', {
+		slidesPerView: 'auto',
+		spaceBetween: 10,
+		breakpoints: {
+			390: {
+				spaceBetween: 24,
+			},
+			350: {
+				spaceBetween: 5,
+			},
+		},
+		autoplay: {
+			delay: 5e3,
+			disableOnInteraction: !1,
+		},
+	})
+})
